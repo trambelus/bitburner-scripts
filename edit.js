6da33ac2194a8f0b876e1e91ae0673aa5f403f84
Trambelus <9337657+trambelus@users.noreply.github.com>
2022-05-06 00:20:23 -0400

    Add refresh flag to editor

--- edit.js
+++ edit.js
@@ -2,15 +2,22 @@ import { editPlayerData } from './skip'
 
 const reloadDelay = 1500
 
+const argsSchema = [
+  ['refresh', false] // true to automatically refresh the game after editing the save
+]
+
 /** @param {import(".").NS} ns */
 export async function main (ns) {
-  const evalStr = ns.args.join(' ')
+  const options = ns.flags(argsSchema)
+  const evalStr = options._.join(' ')
   ns.print(`Evaluating '${evalStr}'`)
   await editPlayerData(playerData => {
     /* eslint-disable-next-line no-eval */
     eval(evalStr)
   })
   // delay to avoid reloading/exiting before save edit is complete
-  // setTimeout(() => { getWindow().location.reload() }, reloadDelay)
-  await ns.sleep(10e3 + reloadDelay)
+  if (options.refresh) {
+    setTimeout(() => { globalThis.location.reload() }, reloadDelay)
+    await ns.sleep(10e3 + reloadDelay)
+  }
 }
