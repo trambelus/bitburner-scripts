ab074efeeca30fc5c8a7b801565a8a3563e9bfce
Trambelus <9337657+trambelus@users.noreply.github.com>
2022-05-06 00:51:23 -0400

    Add gym helper script

---
+++ gym.js
@@ -0,0 +1,140 @@
+// gym.js
+import { log, getNsDataThroughFile } from './helpers'
+
+let _ns
+const doc = [].map.constructor('return this.document')()
+
+const argsSchema = [
+  ['str', Infinity], // strength score target
+  ['def', Infinity], // defense score target
+  ['dex', Infinity], // dexterity score target
+  ['agi', Infinity], // agility score target
+  ['period', '10s'], // time spent on each before cycling to the next
+  ['gym', 'Powerhouse Gym'] // gym to train at. not sure why you would ever change this.
+]
+
+const gymLocations = {
+  'Crush Fitness Gym': 'Aevum',
+  'Snap Fitness Gym': 'Aevum',
+  'Iron Gym': 'Sector-12',
+  'Powerhouse Gym': 'Sector-12',
+  'Millenium Fitness Gym': 'Volhaven'
+}
+
+export function autocomplete (data) {
+  data.flags(argsSchema)
+  return []
+}
+
+function parseTime (timeStr) {
+  let ret = 0
+  const years = timeStr.match(/(\d+)\s*y/)
+  const days = timeStr.match(/(\d+)\s*d/)
+  const hours = timeStr.match(/(\d+)\s*h/)
+  const minutes = timeStr.match(/(\d+)\s*m/)
+  const seconds = timeStr.match(/(\d+)\s*s/)
+  if (years) { ret += parseInt(years[1]) * 60 * 60 * 24 * 365 }
+  if (days) { ret += parseInt(days[1]) * 60 * 60 * 24 }
+  if (hours) { ret += parseInt(hours[1]) * 60 * 60 }
+  if (minutes) { ret += parseInt(minutes[1]) * 60 }
+  if (seconds) { ret += parseInt(seconds[1]) }
+  return ret * 1000
+}
+
+// shorthand function for finding an element by querySelector and filtering by text
+export function queryFilter (query, filter) {
+  return [...doc.querySelectorAll(query)].find(e => e.innerText.trim().match(filter))
+}
+
+async function startGymTraining (stat, gym) {
+  return await getNsDataThroughFile(_ns, 'ns.singularity.gymWorkout(ns.args[0], ns.args[1], ns.args[2])', '/Temp/gym-workout.txt', [gym, stat, false])
+}
+
+async function ensureCity (player, targetCity) {
+  if (player.city !== targetCity) {
+    if (player.money < 200000 || !(await getNsDataThroughFile(_ns, 'ns.travelToCity(ns.args[0])', '/Temp/travel-to-city.txt', [targetCity]))) {
+      return false
+    }
+    await _ns.asleep(1000)
+  }
+  return true
+}
+
+export async function doGainz (strTarget, defTarget, dexTarget, agiTarget, period, gym) {
+  // Validate gym
+  if (!(gym in gymLocations)) {
+    log(_ns, `ERROR: unknown gym '${gym}'`)
+    return
+  }
+  const targetCity = gymLocations[gym]
+
+  let player = await getNsDataThroughFile(_ns, 'ns.getPlayer()', '/Temp/player-info.txt')
+
+  const targetStats = {
+    strength: strTarget,
+    defense: defTarget,
+    dexterity: dexTarget,
+    agility: agiTarget
+  }
+
+  // Allow the user to stop this script by clicking the "Stop training" button in the UI
+  let cancel = false
+  const cancelHook = function () {
+    const btn = queryFilter('button', 'Stop training at gym')
+    if (!btn) return
+    const fn = btn.onclick
+    if (fn._hooked) return
+    btn.onclick = () => {
+      log(_ns, 'Stopping training...')
+      cancel = true
+      fn()
+    }
+    btn.onclick._hooked = true
+    log(_ns, 'Hooked cancel button.')
+  }
+
+  const interval = setInterval(cancelHook, 100)
+
+  try {
+    /* eslint-disable-next-line no-unmodified-loop-condition */
+    while (!cancel) {
+      log(_ns, `Current target stats: ${Object.keys(targetStats).join(', ')}`)
+      for (const stat in targetStats) {
+        if (cancel) break
+        if (targetStats[stat] < player[stat]) {
+          log(_ns, `Target reached for ${stat}!`)
+          delete targetStats[stat]
+          continue
+        }
+        if (!(await ensureCity(player, targetCity))) {
+          log(`ERROR: could not travel to ${targetCity}. Exiting...`)
+          return
+        }
+        player = await getNsDataThroughFile(_ns, 'ns.getPlayer()', '/Temp/player-info.txt')
+        log(_ns, `Training ${stat}, target ${targetStats[stat]}`)
+        const result = await startGymTraining(stat, gym)
+        if (result === false) {
+          log(_ns, 'WARN: gym training failed, probably due to unexpected traveling')
+          continue
+        }
+        await _ns.asleep(period)
+      }
+      if (Object.keys(targetStats).length === 0) {
+        log(_ns, 'All stat targets have been reached. Exiting...')
+        return
+      }
+      await _ns.asleep(0)
+    }
+  } finally {
+    clearInterval(interval)
+  }
+}
+
+/** @param {NS} ns */
+export async function main (ns) {
+  _ns = ns
+  _ns.disableLog('ALL')
+  const options = ns.flags(argsSchema)
+  const period = parseTime(options.period)
+  await doGainz(options.str, options.def, options.dex, options.agi, period, options.gym)
+}
